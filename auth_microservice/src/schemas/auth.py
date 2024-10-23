b342284dc728a3a57a6fcd4321cad971654e8a6a
Egor Konevtsov <konevtsov.code@gmail.com>
2024-10-23 13:48:23 +0700

    feature: Add JWTToken and TokenData schemas

--- auth_microservice/src/schemas/auth.py
+++ auth_microservice/src/schemas/auth.py
@@ -14,3 +14,12 @@ class SignInRequest(BaseModel):
 class CredentialsResponse(BaseModel):
     access_token: str
     refresh_token: str
+
+
+class JWTToken(BaseModel):
+    username: str
+    refresh_token: str
+
+
+class TokenData(BaseModel):
+    username: str
