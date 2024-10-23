26a632884beb03b20ac351fe8564f39bb30cad1f
Egor Konevtsov <konevtsov.code@gmail.com>
2024-10-23 13:52:10 +0700

    fix: Add decoding bytes into a string to be stored in db

--- auth_microservice/src/services/password_service.py
+++ auth_microservice/src/services/password_service.py
@@ -5,17 +5,20 @@ class PasswordService:
     @staticmethod
     def hash_password(
         password: str,
-    ) -> bytes:
-        salt = bcrypt.gensalt()
-        pwd_bytes: bytes = password.encode()
-        return bcrypt.hashpw(pwd_bytes, salt)
+    ) -> str:
+        pwd_bytes: bytes = password.encode('utf-8')
+        pwd_salt: bytes = bcrypt.gensalt()
+        hash_bytes: bytes = bcrypt.hashpw(pwd_bytes, pwd_salt)
+        hash_str = hash_bytes.decode('utf-8')
+
+        return hash_str
 
     @staticmethod
     def validate_password(
         password: str,
-        hashed_password: bytes,
+        hashed_password: str,
     ) -> bool:
-        return bcrypt.checkpw(
-            password=password.encode(),
-            hashed_password=hashed_password,
-        )
+        password_bytes: bytes = password.encode('utf-8')
+        hash_bytes: bytes = hashed_password.encode('utf-8')
+
+        return bcrypt.checkpw(password_bytes, hash_bytes)
