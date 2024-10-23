780ba5ed0fd6630771940eca6eb6eb2f12c9c6b1
Egor Konevtsov <konevtsov.code@gmail.com>
2024-10-23 13:48:42 +0700

    feature: Add sign_in function

--- auth_microservice/src/services/auth_service.py
+++ auth_microservice/src/services/auth_service.py
@@ -8,10 +8,14 @@ from schemas.auth import (
     SignUpRequest,
     SignInRequest,
     CredentialsResponse,
+    JWTToken,
+    TokenData,
 )
 from schemas.user import UserSchema
 from excepltions.auth import (
     UserAlreadyExistsException,
+    UserNotFoundException,
+    WrongPasswordException,
 )
 
 
@@ -22,26 +26,40 @@ class AuthService:
         repository: UserRepository = Depends(UserRepository),
         token_service: TokenService = Depends(TokenService),
     ):
-        self.repository = repository
+        self._repository = repository
         self._password_service = password_service
         self._token_service = token_service
 
     async def sign_up(self, request: SignUpRequest) -> CredentialsResponse:
-        if await self.repository.username_exists(request.username):
+        if await self._repository.username_exists(request.username):
             raise UserAlreadyExistsException
 
         password_hash = self._password_service.hash_password(request.password)
+        token_data = TokenData(username=request.username)
+        refresh_token = self._token_service.create_refresh_token(token_data)
+        access_token = self._token_service.create_access_token(token_data)
 
         new_user = UserSchema(
             username=request.username,
             password=str(password_hash),
-        )
-        await self.repository.create_user(new_user)
-        refresh_token = self._token_service.create_refresh_token(new_user)
-        access_token = self._token_service.create_access_token(new_user)
-        await self.repository.update_refresh_token_by_username(
-            username=new_user.username,
             refresh_token=refresh_token,
+        )
+        await self._repository.create_user(new_user)
+
+        return CredentialsResponse(access_token=access_token, refresh_token=refresh_token)
+
+    async def sign_in(self, request: SignInRequest) -> CredentialsResponse:
+        user = await self._repository.get_user_by_username(request.username)
+        if not user:
+            raise UserNotFoundException
+        if not self._password_service.validate_password(request.password, user.password):
+            raise WrongPasswordException
+
+        token_data = TokenData(username=request.username)
+        refresh_token = self._token_service.create_refresh_token(token_data)
+        access_token = self._token_service.create_access_token(token_data)
+        await self._repository.update_refresh_token_by_username(
+            JWTToken(username=request.username, refresh_token=refresh_token)
         )
 
         return CredentialsResponse(access_token=access_token, refresh_token=refresh_token)
