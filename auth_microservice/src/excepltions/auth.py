2460c5870d1489e14b1a41699dd0b31d280f6680
Egor Konevtsov <konevtsov.code@gmail.com>
2024-10-23 13:46:51 +0700

    feature(exc): Add new exceptions
    Add UserNotFound and WrongPassword exceptions

--- auth_microservice/src/excepltions/auth.py
+++ auth_microservice/src/excepltions/auth.py
@@ -5,3 +5,14 @@ UserAlreadyExistsException = HTTPException(
     status_code=status.HTTP_409_CONFLICT,
     detail="User already exists",
 )
+
+UserNotFoundException = HTTPException(
+    status_code=status.HTTP_404_NOT_FOUND,
+    detail="User not found",
+)
+
+WrongPasswordException = HTTPException(
+    status_code=status.HTTP_401_UNAUTHORIZED,
+    detail="Wrong password",
+)
+
