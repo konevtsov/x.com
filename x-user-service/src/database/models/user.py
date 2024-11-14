4abfac525f1cfe4ecaaea576a38c1341764653da
Egor Konevtsov <konevtsov.code@gmail.com>
2024-11-14 22:55:36 +0700

    feat(user): Create base file structure

---
+++ x-user-service/src/database/models/user.py
@@ -0,0 +1,12 @@
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, Boolean, Text
+
+from .base import Base
+
+
+class User(Base):
+    username: Mapped[str] = mapped_column(String(32), unique=True)
+    email: Mapped[str] = mapped_column(String, unique=True)
+    is_active: Mapped[bool] = mapped_column(Boolean, default=True)
+    about_me: Mapped[str] = mapped_column(String)
+    banned: Mapped[bool] = mapped_column(Boolean, default=False)
