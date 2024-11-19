96be480fbc7ce291dcf7cc48c23e9f5f76444a0f
Egor Konevtsov <konevtsov.code@gmail.com>
2024-11-19 22:33:13 +0700

    feat(post): Add alembic for migrations

--- x-post-service/src/database/models/user.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy.orm import Mapped, mapped_column
-from sqlalchemy import String, Boolean, Text
-
-from .base import Base
-
-
-class User(Base):
-    username: Mapped[str] = mapped_column(String(32), unique=True)
