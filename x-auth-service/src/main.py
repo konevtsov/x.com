fd85dd75a976ec25b8a1738e4b3d0c105351fffd
Egor Konevtsov <konevtsov.code@gmail.com>
2024-11-24 00:52:05 +0700

    feat(auth): Add logging

--- x-auth-service/src/main.py
+++ x-auth-service/src/main.py
@@ -1,9 +1,16 @@
+import logging
+
 import uvicorn
 
 from configuration.config import settings
-
 from routes import api_router
 from create_fastapi_app import create_app
+
+
+logging.basicConfig(
+    level=settings.logging.log_level,
+    format=settings.logging.log_format,
+)
 
 main_app = create_app(
     title=settings.run.title,
