ec10c42d06a1fc7acf26d142cd32af0184aeaa00
Egor Konevtsov <konevtsov.code@gmail.com>
2024-11-25 23:35:30 +0700

    feat(user): Add name, bio and website fields

--- x-user-service/src/database/models/user.py
+++ x-user-service/src/database/models/user.py
@@ -1,12 +1,14 @@
 from sqlalchemy.orm import Mapped, mapped_column
-from sqlalchemy import String, Boolean, Text
+from sqlalchemy import Boolean, Text, String
 
 from .base import Base
 
 
 class User(Base):
-    username: Mapped[str] = mapped_column(String(32), unique=True)
-    email: Mapped[str] = mapped_column(String, unique=True)
+    username: Mapped[str] = mapped_column(String(32), unique=True, nullable=False)
+    name: Mapped[str] = mapped_column(String(50), nullable=False)
+    email: Mapped[str] = mapped_column(String(100), unique=True, nullable=False)
     is_active: Mapped[bool] = mapped_column(Boolean, default=True)
-    about_me: Mapped[str] = mapped_column(String)
+    bio: Mapped[str] = mapped_column(String(160))
+    website: Mapped[str] = mapped_column(String(100), nullable=True)
     is_banned: Mapped[bool] = mapped_column(Boolean, default=False)
