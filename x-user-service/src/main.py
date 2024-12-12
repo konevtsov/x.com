88f6739fa78825c75fb64d3ffc57d6bc07e61df3
Egor Konevtsov <konevtsov.code@gmail.com>
2024-12-12 22:45:55 +0700

    feat(user): add CORSMiddleware

--- x-user-service/src/main.py
+++ x-user-service/src/main.py
@@ -1,6 +1,7 @@
 import logging
 
 import uvicorn
+from fastapi.middleware.cors import CORSMiddleware
 
 from configuration.config import settings
 from routes import api_router
@@ -17,6 +18,14 @@ main_app = create_app(
 )
 
 main_app.include_router(api_router)
+
+main_app.add_middleware(
+    CORSMiddleware,
+    allow_origins=settings.cors.allow_origins,
+    allow_methods=settings.cors.allow_methods,
+    allow_headers=settings.cors.allow_headers,
+    allow_credentials=settings.cors.allow_credentials,
+)
 
 if __name__ == "__main__":
     uvicorn.run(
