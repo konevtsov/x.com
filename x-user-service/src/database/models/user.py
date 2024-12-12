0c3da7244ce43738e3fb5d7aebecb9f0a9ab8d99
Egor Konevtsov <konevtsov.code@gmail.com>
2024-12-12 22:43:34 +0700

    feat(user): Change nullable rule for name and bio fields

--- x-user-service/src/database/models/user.py
+++ x-user-service/src/database/models/user.py
@@ -1,14 +1,14 @@
 from sqlalchemy.orm import Mapped, mapped_column
-from sqlalchemy import Boolean, Text, String
+from sqlalchemy import Boolean, String
 
 from .base import Base
 
 
 class User(Base):
     username: Mapped[str] = mapped_column(String(32), unique=True, nullable=False)
-    name: Mapped[str] = mapped_column(String(50), nullable=False)
+    name: Mapped[str] = mapped_column(String(50), nullable=True)
     email: Mapped[str] = mapped_column(String(100), unique=True, nullable=False)
     is_active: Mapped[bool] = mapped_column(Boolean, default=True)
-    bio: Mapped[str] = mapped_column(String(160))
+    bio: Mapped[str] = mapped_column(String(160), nullable=True)
     website: Mapped[str] = mapped_column(String(100), nullable=True)
     is_banned: Mapped[bool] = mapped_column(Boolean, default=False)
