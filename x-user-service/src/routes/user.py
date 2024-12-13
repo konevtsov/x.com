aaf5116964811836f28944c09bf9566a7429ca39
Egor Konevtsov <konevtsov.code@gmail.com>
2024-12-13 23:36:58 +0700

    feat(user): Add update endpoint

--- x-user-service/src/routes/user.py
+++ x-user-service/src/routes/user.py
@@ -1,4 +1,17 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+
+from schemas.user import UserUpdateScheme
+from services.user_service import UserService
 
 
 router = APIRouter(prefix="/users", tags=["Users"])
+
+
+@router.put(
+    path="/",
+)
+async def update_user(
+    user: UserUpdateScheme,
+    user_service: UserService = Depends(UserService),
+):
+    await user_service.update_user(user)
