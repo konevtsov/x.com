0d24017184267ecfa2df187d3e332514e8bca197
Egor Konevtsov <konevtsov.code@gmail.com>
2024-12-28 22:52:56 +0700

    feat(user): Add custom token_exceptions

---
+++ x-user-service/src/exceptions/token_exceptions.py
@@ -0,0 +1,7 @@
+from fastapi import status
+from .base_exception import BaseExceptions
+
+
+class InvalidTokenError(BaseExceptions):
+    status_code: int = status.HTTP_403_FORBIDDEN
+    detail: str = "Invalid token"
