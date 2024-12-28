92072add8ece856ce7d26260ff90ab8f456b4fc8
Egor Konevtsov <konevtsov.code@gmail.com>
2024-12-28 22:54:40 +0700

    feat(user): Add base class for custom exceptions

---
+++ x-user-service/src/exceptions/base_exception.py
@@ -0,0 +1,12 @@
+from fastapi import HTTPException
+
+
+class BaseExceptions(HTTPException):
+
+    def __init__(self, detail=None):
+        self.detail = detail
+        super().__init__(
+            status_code=self.status_code,
+            detail=self.detail,
+        )
+    
