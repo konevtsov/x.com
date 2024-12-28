079fa12549982f7c2f46c5313d81e0abe5aea6b1
Egor Konevtsov <konevtsov.code@gmail.com>
2024-12-28 22:38:06 +0700

    feat(user): Add validation to update_user function

--- x-user-service/src/services/user_service.py
+++ x-user-service/src/services/user_service.py
@@ -1,10 +1,14 @@
 from fastapi import Depends
 
 from schemas.user import (
-    UserUpdateScheme,
-    UserInScheme,
+    UserUpdateRequestSchema,
 )
 from repositories.user_repository import UserRepository
+from schemas.token import TokenIntrospectSchema
+from exceptions.user_exceptions import (
+    UserNotFoundError,
+)
+from dto.user import UserCreateDTO
 
 
 class UserService:
@@ -14,8 +18,11 @@ class UserService:
     ):
         self._repository = repository
 
-    async def create_user(self, user: UserInScheme):
+    async def create_user(self, user: UserCreateDTO):
         await self._repository.create_user(user=user)
 
-    async def update_user(self, user: UserUpdateScheme):
-        await self._repository.update_user(user)
+    async def update_user(self, user_update: UserUpdateRequestSchema, user_token: TokenIntrospectSchema):
+        user = await self._repository.get_user_by_email(email=user_token.email)
+        if not user:
+            raise UserNotFoundError
+        await self._repository.update_user_by_email(user_update, email=user_token.email)
