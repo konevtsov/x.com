9c178c716ef176a3f12f0ddc459d7bd01b3e80d1
Egor Konevtsov <konevtsov.code@gmail.com>
2024-12-28 22:37:13 +0700

    feat(user): Rename file and move to user folder

---
+++ x-user-service/src/routes/v1/user/views.py
@@ -0,0 +1,19 @@
+from fastapi import APIRouter, Depends
+
+from schemas.user import UserUpdateRequestSchema
+from services.user_service import UserService
+from .token_introspection import get_token_info_from_current_user
+from schemas.token import TokenIntrospectSchema
+
+router = APIRouter(prefix="/users", tags=["Users"])
+
+
+@router.put(
+    path="/",
+)
+async def update_user(
+    user_update: UserUpdateRequestSchema,
+    user_token: TokenIntrospectSchema = Depends(get_token_info_from_current_user),
+    user_service: UserService = Depends(UserService),
+):
+    await user_service.update_user(user_update, user_token)
