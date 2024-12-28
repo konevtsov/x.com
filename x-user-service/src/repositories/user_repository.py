20e160cd081d6e064bd808ef33bc282d0364d1b1
Egor Konevtsov <konevtsov.code@gmail.com>
2024-12-28 22:39:57 +0700

    feat(user): Change search and update functions from username to email

--- x-user-service/src/repositories/user_repository.py
+++ x-user-service/src/repositories/user_repository.py
@@ -1,30 +1,33 @@
-from typing import AsyncGenerator
-
 from fastapi import Depends
-from sqlalchemy import select, update, insert, delete
+from sqlalchemy import select, update, insert, delete, Result
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from database.models import User
 from database.session import connector
 from schemas.user import (
-    UserUpdateScheme,
-    UserInScheme,
+    UserUpdateRequestSchema,
 )
+from dto.user import UserCreateDTO
 
 
 class UserRepository:
     def __init__(self, session: AsyncSession = Depends(connector.session_getter)):
         self._session = session
 
-    async def create_user(self, user: UserInScheme):
+    async def create_user(self, user: UserCreateDTO):
         new_user = User(**user.model_dump())
         self._session.add(new_user)
         await self._session.commit()
 
-    async def update_user(self, user: UserUpdateScheme):
+    async def get_user_by_email(self, email: str):
+        stmt = select(User).where(User.email == email)
+        result: Result = await self._session.execute(stmt)
+        return result.scalar()
+
+    async def update_user_by_email(self, user: UserUpdateRequestSchema, email: str):
         stmt = (
             update(User).
-            where(User.username == user.username).
+            where(User.email == email).
             values(**user.model_dump())
         )
         await self._session.execute(stmt)
