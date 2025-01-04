5be62582d5890fdd1801d5b07aec7c74b41deeb1
Egor Konevtsov <konevtsov.code@gmail.com>
2025-01-05 00:30:41 +0700

    feat(auth)!: Change auth logic
    BREAKING CHANGES: Now refresh token stored in cookie

--- x-auth-service/src/services/auth_service.py
+++ x-auth-service/src/services/auth_service.py
@@ -1,5 +1,8 @@
+import uuid
+
 from fastapi import Depends
 
+from repositories.refresh_session_repository import RefreshSessionRepository
 from .password_service import PasswordService
 from repositories.user import UserRepository
 from .token_service import (
@@ -15,7 +18,7 @@ from schemas.auth import (
     IntrospectResponseSchema,
     JWTTokenUpdateSchema,
     TokenDataSchema,
-    TokenResponseSchema,
+    TokenResponseSchema, RefreshSessionSchema,
 )
 from dto.user import UserCreateDTO
 
@@ -36,15 +39,17 @@ class AuthService:
     def __init__(
         self,
         password_service: PasswordService = Depends(PasswordService),
-        repository: UserRepository = Depends(UserRepository),
+        user_repository: UserRepository = Depends(UserRepository),
+        refresh_session_repository: RefreshSessionRepository = Depends(RefreshSessionRepository),
         token_service: TokenService = Depends(TokenService),
     ):
-        self._repository = repository
+        self._user_repository = user_repository
+        self._refresh_session_repository = refresh_session_repository
         self._password_service = password_service
         self._token_service = token_service
 
     async def sign_up(self, request: SignUpRequestSchema) -> None:
-        if await self._repository.email_exists(request.email):
+        if await self._user_repository.email_exists(request.email):
             raise UserAlreadyExists
 
         password_hash = self._password_service.hash_password(request.password)
@@ -53,7 +58,7 @@ class AuthService:
             username=request.username,
             password=str(password_hash),
         )
-        await self._repository.create_user(new_user)
+        await self._user_repository.create_user(new_user)
 
         await user_mq.publish_message(
             data=UserCreateDTO(email=request.email, username=request.username),
@@ -61,55 +66,61 @@ class AuthService:
         )
 
     async def sign_in(self, request: SignInRequestSchema) -> TokenResponseSchema:
-        user = await self._repository.get_user_by_email(request.email)
+        user = await self._user_repository.get_user_by_email(request.email)
         if not user:
             raise UserNotFound
         if not self._password_service.validate_password(request.password, user.password):
             raise InvalidLoginPassword
 
         token_data = TokenDataSchema(email=request.email, username=user.username)
+        refresh_token_uuid = str(uuid.uuid4())
         refresh_token = self._token_service.create_refresh_token(token_data)
         access_token = self._token_service.create_access_token(token_data)
-        await self._repository.update_refresh_token_by_email(
-            JWTTokenUpdateSchema(email=request.email, refresh_token=refresh_token)
+        refresh_session_schema = RefreshSessionSchema(
+            user_id=user.id, refresh_token_uuid=refresh_token_uuid, refresh_token=refresh_token, ip=request.ip,
         )
+        if len(await self._refresh_session_repository.get_all_sessions_by_user_id(user_id=user.id)) >= 5:
+            await self._refresh_session_repository.drop_all_sessions(user_id=user.id)
+        await self._refresh_session_repository.add_refresh_session(refresh_session_schema)
 
-        return TokenResponseSchema(access_token=access_token, refresh_token=refresh_token)
+        return TokenResponseSchema(access_token=access_token, refresh_token=refresh_token_uuid)
 
-    async def sign_out(self, jwt_token: str) -> None:
+    async def sign_out(self, refresh_token_uuid: str) -> None:
+        jwt_token = await self._refresh_session_repository.get_token_by_uuid(refresh_token_uuid)
         token_payload = self._token_service.get_token_payload(jwt_token)
         self._token_service.validate_token_type(token_payload, REFRESH_TOKEN_TYPE)
 
         email = token_payload.get(TOKEN_SUBJECT_FIELD)
-        refresh_token = await self._repository.get_token_by_email(email=email)
+        all_refresh_tokens = await self._refresh_session_repository.get_all_refresh_tokens_by_email(email=email)
 
-        if not refresh_token or refresh_token != jwt_token:
+        if not all_refresh_tokens or jwt_token not in all_refresh_tokens:
             raise Unauthorized
-        await self._repository.delete_token_by_email(email=email)
+        await self._refresh_session_repository.delete_session_by_uuid(refresh_token_uuid)
 
-    async def refresh(self, jwt_token: str) -> TokenResponseSchema:
+    async def refresh(self, refresh_token_uuid: str) -> TokenResponseSchema:
         """
         Refresh access token
-        :param jwt_token: JWT token from Authorization header
-        :return: New access token
+        :param refresh_token_uuid: JWT token uuid from cookie
+        :return: New access and refresh tokens
         """
+        jwt_token = await self._refresh_session_repository.get_token_by_uuid(refresh_token_uuid)
         token_payload = self._token_service.get_token_payload(jwt_token)
         self._token_service.validate_token_type(token_payload, REFRESH_TOKEN_TYPE)
 
         email = token_payload.get(TOKEN_SUBJECT_FIELD)
         username = token_payload.get(TOKEN_USERNAME_FIELD)
-        old_refresh_token = await self._repository.get_token_by_email(email)
+        all_refresh_tokens = await self._refresh_session_repository.get_all_refresh_tokens_by_email(email=email)
 
-        if not old_refresh_token or old_refresh_token != jwt_token:
+        if not all_refresh_tokens or jwt_token not in all_refresh_tokens:
             raise Unauthorized
 
         token_data = TokenDataSchema(email=email, username=username)
         access_token = self._token_service.create_access_token(token_data)
         refresh_token = self._token_service.create_refresh_token(token_data)
-        await self._repository.update_refresh_token_by_email(
-            JWTTokenUpdateSchema(email=email, refresh_token=refresh_token)
+        await self._refresh_session_repository.update_refresh_token_by_uuid(
+            JWTTokenUpdateSchema(uuid=refresh_token_uuid, refresh_token=refresh_token)
         )
-        return TokenResponseSchema(access_token=access_token, refresh_token=refresh_token)
+        return TokenResponseSchema(access_token=access_token, refresh_token=refresh_token_uuid)
 
     async def introspect(self, jwt_token: str) -> IntrospectResponseSchema:
         token_payload = self._token_service.get_token_payload(jwt_token)
