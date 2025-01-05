e6d2ba5e6a2d6d15f9f3636cc44fe7b04a2f75bc
Egor Konevtsov <konevtsov.code@gmail.com>
2025-01-06 02:21:00 +0700

    feat(user): Add handler for showing user by username

--- x-user-service/src/routes/v1/user/views.py
+++ x-user-service/src/routes/v1/user/views.py
@@ -1,11 +1,23 @@
 from fastapi import APIRouter, Depends
 
 from schemas.user import UserUpdateRequestSchema
+from services import user_service
 from services.user_service import UserService
 from .token_introspection import get_token_info_from_current_user
 from schemas.token import TokenIntrospectSchema
 
 router = APIRouter(tags=["Users"])
+
+
+@router.get(
+    path="/{username}",
+)
+async def get_user_by_username(
+    username: str,
+    user_token: TokenIntrospectSchema = Depends(get_token_info_from_current_user),
+    user_service: UserService = Depends(UserService),
+):
+    return await user_service.get_user_by_username(username=username, user_token=user_token)
 
 
 @router.put(
