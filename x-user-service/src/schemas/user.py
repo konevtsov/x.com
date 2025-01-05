0dc3cf8a8277109264897853926c7e1056092d60
Egor Konevtsov <konevtsov.code@gmail.com>
2025-01-06 02:20:03 +0700

    feat(user): Add schemas for showing user profile

--- x-user-service/src/schemas/user.py
+++ x-user-service/src/schemas/user.py
@@ -1,14 +1,31 @@
+from typing import Optional
+from datetime import datetime
+
 from pydantic import BaseModel, EmailStr
 
 
-class UserScheme(BaseModel):
+class FullUserSchema(BaseModel):
     username: str
-    name: str
+    name: Optional[str] = None  # TODO: Change to required
     email: EmailStr
-    is_active: bool
-    bio: str
-    website: str
+    bio: Optional[str] = None
+    website: Optional[str] = None
     is_banned: bool
+    created_at: datetime
+
+    class Config:
+        from_attributes = True
+
+
+class PartialUserSchema(BaseModel):
+    username: str
+    name: Optional[str] = None  # TODO: Change to required
+    bio: Optional[str] = None
+    website: Optional[str] = None
+    created_at: datetime
+
+    class Config:
+        from_attributes = True
 
 
 class UserUpdateRequestSchema(BaseModel):
