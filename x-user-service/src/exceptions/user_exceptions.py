a958922721c669581c656d7a7d3c2aa1b87c4a78
Egor Konevtsov <konevtsov.code@gmail.com>
2025-01-07 19:38:18 +0700

    feat(user): Add FollowYourselfError

--- x-user-service/src/exceptions/user_exceptions.py
+++ x-user-service/src/exceptions/user_exceptions.py
@@ -6,3 +6,8 @@ from .base_exception import BaseExceptions
 class UserNotFoundError(BaseExceptions):
     status_code: int = status.HTTP_404_NOT_FOUND
     detail: str = "User not found"
+
+
+class FollowYourselfError(BaseExceptions):
+    status_code: int = status.HTTP_409_CONFLICT
+    detail: str = "You can't follow yourself"
