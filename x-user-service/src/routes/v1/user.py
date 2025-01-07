f0d0f83088d0e7b498e494676ac3a357c896b024
Egor Konevtsov <konevtsov.code@gmail.com>
2025-01-07 19:36:40 +0700

    feat(user): Add /follow view

--- x-user-service/src/routes/v1/user.py
+++ x-user-service/src/routes/v1/user.py
@@ -1,7 +1,6 @@
 from fastapi import APIRouter, Depends
 
-from schemas.user import UserUpdateRequestSchema
-from services import user_service
+from schemas.user import UserUpdateRequestSchema, FollowRequestSchema, FollowSchema
 from services.user_service import UserService
 from .token_introspection import get_token_info_from_current_user
 from schemas.token import TokenIntrospectSchema
@@ -18,6 +17,22 @@ async def get_user_by_username(
     user_service: UserService = Depends(UserService),
 ):
     return await user_service.get_user_by_username(username=username, user_token=user_token)
+
+
+@router.post(
+    path="/follow"
+)
+async def follow(
+    follow_request: FollowRequestSchema,
+    user_token: TokenIntrospectSchema = Depends(get_token_info_from_current_user),
+    user_service: UserService = Depends(UserService),
+):
+    follow_schema = FollowSchema(
+        followed_username=user_token.username,
+        follower_username=follow_request.username,
+    )
+
+    await user_service.follow(follow_schema=follow_schema)
 
 
 @router.put(
