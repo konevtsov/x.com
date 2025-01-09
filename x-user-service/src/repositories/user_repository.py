e62f5fd68f38f774a5c23fa92df019cdbbc8a522
Egor Konevtsov <konevtsov.code@gmail.com>
2025-01-09 16:53:13 +0700

    feat(user): Add selectinloads to function for getting user by username

--- x-user-service/src/repositories/user_repository.py
+++ x-user-service/src/repositories/user_repository.py
@@ -1,8 +1,10 @@
-from sqlite3 import IntegrityError
+import logging
 
 from fastapi import Depends
 from sqlalchemy import select, update, insert, delete, Result, text
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.orm import selectinload
+from sqlalchemy.exc import IntegrityError
 
 from database.models import User, Follow
 from database.session import connector
@@ -17,17 +19,17 @@ class UserRepository:
     def __init__(self, session: AsyncSession = Depends(connector.session_getter)):
         self._session = session
 
-    async def create_user(self, user: UserCreateDTO):
+    async def create_user(self, user: UserCreateDTO) -> None:
         new_user = User(**user.model_dump())
         self._session.add(new_user)
         await self._session.commit()
 
-    async def get_user_by_email(self, email: str):
+    async def get_user_by_email(self, email: str) -> User:
         stmt = select(User).where(User.email == email)
         result: Result = await self._session.execute(stmt)
         return result.scalar()
 
-    async def update_user_by_email(self, user: UserUpdateRequestSchema, email: str):
+    async def update_user_by_email(self, user: UserUpdateRequestSchema, email: str) -> None:
         stmt = (
             update(User).
             where(User.email == email).
@@ -36,17 +38,24 @@ class UserRepository:
         await self._session.execute(stmt)
         await self._session.commit()
 
-    async def get_user_by_username(self, username: str):
-        stmt = select(User).where(User.username == username)
+    async def get_user_by_username(self, username: str) -> User | None:
+        stmt = (
+            select(User)
+            .options(
+                selectinload(User.followers),
+                selectinload(User.following),
+            )
+            .where(User.username == username)
+        )
         result: Result = await self._session.execute(stmt)
         return result.scalar()
 
-    async def get_user_id_by_username(self, username: str):
+    async def get_user_id_by_username(self, username: str) -> int | None:
         stmt = select(User.id).where(User.username == username)
         result: Result = await self._session.execute(stmt)
         return result.scalar()
 
-    async def follow_by_username(self, followed_id: int, follower_id: int):
+    async def follow_by_username(self, followed_id: int, follower_id: int) -> None:
         stmt = insert(Follow).values(followed_id=followed_id, follower_id=follower_id)
         try:
             await self._session.execute(stmt)
@@ -54,7 +63,7 @@ class UserRepository:
             raise AlreadyFollowedError
         await self._session.commit()
 
-    async def unfollow_by_username(self, followed_id: int, follower_id: int):
+    async def unfollow_by_username(self, followed_id: int, follower_id: int) -> None:
         stmt = delete(Follow).where(Follow.followed_id == followed_id).where(Follow.follower_id == follower_id)
         await self._session.execute(stmt)
         await self._session.commit()
