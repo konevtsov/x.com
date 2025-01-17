62f23ae45922e7fd76b9b4a5dc4e850833dd5c12
Egor Konevtsov <konevtsov.code@gmail.com>
2025-01-18 00:27:54 +0700

    feat(post): Add create_post view

--- x-post-service/src/routes/v1/__init__.py
+++ x-post-service/src/routes/v1/__init__.py
@@ -4,5 +4,7 @@ __all__ = (
 from fastapi import APIRouter
 
 from configuration.config import settings
+from .post import router as post_router
 
 v1_router = APIRouter(prefix=settings.api.v1.prefix)
+v1_router.include_router(post_router, prefix=settings.api.v1.posts)
