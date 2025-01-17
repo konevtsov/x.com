8322c756e4eba7416f66b42f05368b4818eea08c
Egor Konevtsov <konevtsov.code@gmail.com>
2025-01-18 00:35:20 +0700

    feat(user): Change default run config

--- x-user-service/src/configuration/config.py
+++ x-user-service/src/configuration/config.py
@@ -17,9 +17,9 @@ BASE_DIR = Path(__file__).parent.parent
 
 
 class RunConfig(BaseModel):
-    title: str = "X.com"
+    title: str = "X.com User"
     host: str = "127.0.0.1"
-    port: int = 8000
+    port: int = 8001
 
 
 class CorsSettings(BaseModel):
