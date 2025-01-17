9292e2d4318dff07ae7223ed3b92c15558e32cac
Egor Konevtsov <konevtsov.code@gmail.com>
2025-01-18 00:24:51 +0700

    feat(post): Add function for token introspection

---
+++ x-post-service/src/routes/v1/token_introspection.py
@@ -0,0 +1,25 @@
+from aiohttp import ClientSession
+from fastapi import Depends
+from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
+
+from configuration.config import settings
+from exceptions.token_exceptions import InvalidTokenError
+from schemas.token import TokenIntrospectSchema
+
+
+http_bearer = HTTPBearer()
+
+
+async def get_token_info_from_current_user(
+    credentials: HTTPAuthorizationCredentials = Depends(http_bearer),
+):
+    jwt_token = credentials.credentials
+    headers = {'Authorization': f'Bearer {jwt_token}'}
+    async with ClientSession() as session:
+        async with session.get(
+            url=settings.auth_api.introspect_url,
+            headers=headers,
+        ) as response:
+            if not response.ok:
+                raise InvalidTokenError
+            return TokenIntrospectSchema(**await response.json())
