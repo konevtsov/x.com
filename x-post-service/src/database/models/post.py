82898c9109a2c0294f19e45f0cb7800fa9c51870
Egor Konevtsov <konevtsov.code@gmail.com>
2025-01-18 23:46:47 +0700

    feat(post): Remove unique settings from author columns

--- x-post-service/src/database/models/post.py
+++ x-post-service/src/database/models/post.py
@@ -5,6 +5,6 @@ from .base import Base
 
 
 class Post(Base):
-    author_username: Mapped[str] = mapped_column(String(32), unique=True)
-    author_email: Mapped[str] = mapped_column(Text, unique=True)
+    author_username: Mapped[str] = mapped_column(String(32), unique=False)
+    author_email: Mapped[str] = mapped_column(Text, unique=False)
     text: Mapped[str] = mapped_column(String(280))
