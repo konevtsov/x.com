05b98de28a91203276e91c49cb0ce6b12c03c754
Egor Konevtsov <konevtsov.code@gmail.com>
2025-01-18 23:47:37 +0700

    feat(post): Add PostNotFound error

---
+++ x-post-service/src/exceptions/post_exceptions.py
@@ -0,0 +1,7 @@
+from fastapi import status
+from .base_exception import BaseExceptions
+
+
+class PostNotFoundError(BaseExceptions):
+    status_code: int = status.HTTP_404_NOT_FOUND
+    detail: str = "Post not found"
