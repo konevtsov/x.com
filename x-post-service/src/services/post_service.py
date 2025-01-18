de975012f4ec3c6bf3325bbc2a7c3fca6a11af4c
Egor Konevtsov <konevtsov.code@gmail.com>
2025-01-18 23:48:07 +0700

    feat(post): Add delete_post function

--- x-post-service/src/services/post_service.py
+++ x-post-service/src/services/post_service.py
@@ -1,7 +1,9 @@
 from fastapi import Depends
 
 from repositories.post_repository import PostRepository
-from schemas.post import PostCreateSchema
+from schemas.post import PostCreateSchema, PostDeleteSchema
+from exceptions.base_exception import PermissionDeniedError
+from exceptions.post_exceptions import PostNotFoundError
 
 
 class PostService:
@@ -13,3 +15,11 @@ class PostService:
 
     async def create_post(self, post_create: PostCreateSchema):
         await self._post_repository.create_post(post_create)
+
+    async def delete_post(self, post_delete: PostDeleteSchema):
+        post = await self._post_repository.get_post_by_id(post_id=post_delete.post_id)
+        if not post:
+            raise PostNotFoundError
+        if post.author_email != post_delete.author_email:
+            raise PermissionDeniedError
+        await self._post_repository.delete_post_by_id(post_id=post_delete.post_id)
