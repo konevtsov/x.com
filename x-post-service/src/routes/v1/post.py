dce58371071d9bca8c068ec18ec516f903e3fd0b
Egor Konevtsov <konevtsov.code@gmail.com>
2025-01-18 23:47:09 +0700

    feat(post): Add post_delete view

--- x-post-service/src/routes/v1/post.py
+++ x-post-service/src/routes/v1/post.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Depends
 
-from schemas.post import PostCreateRequestSchema, PostCreateSchema
+from schemas.post import PostCreateRequestSchema, PostCreateSchema, PostDeleteRequestSchema, PostDeleteSchema
 from schemas.token import TokenIntrospectSchema
 from services.post_service import PostService
 from .token_introspection import get_token_info_from_current_user
@@ -21,4 +21,19 @@ async def post_create(
         author_username=user_token.username,
         text=post_create_req.text
     )
-    return await post_service.create_post(post_create_schema)
+    await post_service.create_post(post_create_schema)
+
+
+@router.delete(
+    "/",
+)
+async def post_delete(
+    post_delete_req: PostDeleteRequestSchema,
+    user_token: TokenIntrospectSchema = Depends(get_token_info_from_current_user),
+    post_service: PostService = Depends(PostService),
+):
+    post_delete_schema = PostDeleteSchema(
+        author_email=user_token.email,
+        post_id=post_delete_req.post_id,
+    )
+    await post_service.delete_post(post_delete_schema)
