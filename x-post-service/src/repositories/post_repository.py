f50c2443cc2c4f2ff0860d2986c7eb781e9f7071
Egor Konevtsov <konevtsov.code@gmail.com>
2025-01-18 23:47:55 +0700

    feat(post): Add function for delete post by id

--- x-post-service/src/repositories/post_repository.py
+++ x-post-service/src/repositories/post_repository.py
@@ -1,4 +1,5 @@
 from fastapi import Depends
+from sqlalchemy import select, insert, delete, update, Result
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from database.session import connector
@@ -16,4 +17,14 @@ class PostRepository:
     async def create_post(self, post_create: PostCreateSchema):
         post = Post(**post_create.model_dump())
         self._session.add(post)
+        await self._session.commit()
+
+    async def get_post_by_id(self, post_id: int):
+        stmt = select(Post).where(Post.id == post_id)
+        result: Result = await self._session.execute(stmt)
+        return result.scalar()
+
+    async def delete_post_by_id(self, post_id: int):
+        stmt = delete(Post).where(Post.id == post_id)
+        await self._session.execute(stmt)
         await self._session.commit()
