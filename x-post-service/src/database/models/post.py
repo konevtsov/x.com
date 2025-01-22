148bc70579d49eebf98c40561c454ec773bd741d
Egor Konevtsov <konevtsov.code@gmail.com>
2025-01-22 23:46:00 +0700

    feat(post): Add likes field to Post model

--- x-post-service/src/database/models/post.py
+++ x-post-service/src/database/models/post.py
@@ -1,10 +1,22 @@
-from sqlalchemy.orm import Mapped, mapped_column
+from typing import TYPE_CHECKING
+
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 from sqlalchemy import String, Boolean, Text
 
 from .base import Base
+
+
+if TYPE_CHECKING:
+    from .like import Like
 
 
 class Post(Base):
     author_username: Mapped[str] = mapped_column(String(32), unique=False)
     author_email: Mapped[str] = mapped_column(Text, unique=False)
     text: Mapped[str] = mapped_column(String(280))
+
+    likes: Mapped[list["Like"]] = relationship("Like", back_populates="post")
+
+    @property
+    def likes_count(self) -> int:
+        return len(self.likes)
