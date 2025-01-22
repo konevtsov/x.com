8647f53c15c21a0b4a1e6d43efbffae1b3ae1e23
Egor Konevtsov <konevtsov.code@gmail.com>
2025-01-22 23:49:21 +0700

    feat(post): Add validation all posts in get_all_posts funcion

--- x-post-service/src/services/post_service.py
+++ x-post-service/src/services/post_service.py
@@ -1,9 +1,12 @@
+import orjson
 from fastapi import Depends
 
 from repositories.post_repository import PostRepository
-from schemas.post import PostCreateSchema, PostDeleteSchema
+from schemas.post import PostCreateSchema, PostDeleteSchema, PostSchema
 from exceptions.base_exception import PermissionDeniedError
 from exceptions.post_exceptions import PostNotFoundError
+
+from database.models import Post
 
 
 class PostService:
@@ -25,7 +28,11 @@ class PostService:
         await self._post_repository.delete_post_by_id(post_id=post_delete.post_id)
 
     async def get_all_posts_by_username(self, username: str):
-        posts = await self._post_repository.get_all_posts_by_username(username=username)
+        posts: list[Post] = await self._post_repository.get_all_posts_by_username(username=username)
         if not posts:
             raise PostNotFoundError
-        return posts
+
+        validate_posts = {}
+        for i in range(len(posts)):
+            validate_posts[i] = PostSchema.model_validate(posts[i])
+        return validate_posts
