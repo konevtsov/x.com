40d9a7b2855c3922867edbcc7e0a6e0417e02304
Egor Konevtsov <konevtsov.code@gmail.com>
2025-01-30 23:01:34 +0700

    feat(auth): Add test_db_url field

--- x-auth-service/src/configuration/config.py
+++ x-auth-service/src/configuration/config.py
@@ -72,6 +72,7 @@ class RMQConfig(BaseModel):
 
 class DatabaseConfig(BaseModel):
     url: PostgresDsn
+    test_db_url: PostgresDsn
     echo: bool = False
     echo_pool: bool = False
     pool_size: int = 50
