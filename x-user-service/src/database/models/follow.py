fd55fbf16ba3480de8ea4f7dd729ea7551a6411c
Egor Konevtsov <konevtsov.code@gmail.com>
2025-02-02 22:57:41 +0700

    feat(user): Add on_delete param

--- x-user-service/src/database/models/follow.py
+++ x-user-service/src/database/models/follow.py
@@ -14,8 +14,12 @@ class Follow(Base):
     followed_id: Mapped[int] = mapped_column(Integer, ForeignKey("users.id"))
     follower_id: Mapped[int] = mapped_column(Integer, ForeignKey("users.id"))
 
-    followed_user: Mapped["User"] = relationship("User", foreign_keys=[followed_id], back_populates="followers")
-    follower_user: Mapped["User"] = relationship("User", foreign_keys=[follower_id], back_populates="following")
+    followed_user: Mapped["User"] = relationship(
+        "User", foreign_keys=[followed_id], back_populates="followers", cascade="all, delete-orphan",
+    )
+    follower_user: Mapped["User"] = relationship(
+        "User", foreign_keys=[follower_id], back_populates="following", cascade="all, delete-orphan",
+    )
 
     __table_args__ = (
         UniqueConstraint("followed_id", "follower_id", name="unique_follow"),
