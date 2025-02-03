dea89a03e64628a5aa27c3991709d7578277ffe7
Egor Konevtsov <konevtsov.code@gmail.com>
2025-02-03 23:42:37 +0700

    test(user): Fix tests and remove "src" from import paths

--- x-user-service/tests/test_services/test_user_service.py
+++ x-user-service/tests/test_services/test_user_service.py
@@ -2,17 +2,17 @@ import pytest
 
 from unittest.mock import AsyncMock
 
-from src.services.user_service import UserService
-from src.schemas.token import TokenIntrospectSchema
-from src.schemas.user import UserUpdateRequestSchema, FollowSchema, UnfollowSchema
-from src.exceptions.user_exceptions import (
+from services.user_service import UserService
+from schemas.token import TokenIntrospectSchema
+from schemas.user import UserUpdateRequestSchema, FollowSchema, UnfollowSchema
+from exceptions.user_exceptions import (
     UserNotFoundError,
     FollowYourselfError,
     UnfollowYourselfError,
     AlreadyFollowedError,
 )
-from src.dto.user import UserCreateDTO
-from src.repositories.user_repository import UserRepository, UserCreateDTO
+from dto.user import UserCreateDTO
+from repositories.user_repository import UserRepository, UserCreateDTO
 
 
 @pytest.fixture
@@ -27,7 +27,7 @@ def user_service(mock_user_repository):
 
 @pytest.mark.asyncio
 async def test_create_user(user_service, mock_user_repository):
-    user_data = UserCreateDTO(email="test@example.com", username="test_username")
+    user_data = UserCreateDTO(email="test@example.com", username="test_username", user_id=1)
     await user_service.create_user(user=user_data)
     mock_user_repository.create_user.assert_called_once_with(user=user_data)
 
@@ -40,10 +40,10 @@ async def test_update_user_good(user_service, mock_user_repository):
         bio="test_bio",
         website="test_website",
     )
-    user_token = TokenIntrospectSchema(email="test@example.com", username="test_username")
-    mock_user_repository.get_user_by_email.return_value = AsyncMock()
+    user_token = TokenIntrospectSchema(user_id=1, username="test_username")
+    mock_user_repository.get_user_by_user_id.return_value = AsyncMock()
     await user_service.update_user(user_update, user_token)
-    mock_user_repository.update_user_by_email.assert_awaited_once_with(user_update, email=user_token.email)
+    mock_user_repository.update_user_by_user_id.assert_awaited_once_with(user_update, user_id=1)
 
 
 @pytest.mark.asyncio
@@ -54,8 +54,8 @@ async def test_update_user_bad(user_service, mock_user_repository):
         bio="test_bio",
         website="test_website",
     )
-    user_token = TokenIntrospectSchema(email="test@example.com", username="test_username")
-    mock_user_repository.get_user_by_email.return_value = None
+    user_token = TokenIntrospectSchema(user_id=1, username="test_username")
+    mock_user_repository.get_user_by_user_id.return_value = None
     with pytest.raises(UserNotFoundError) as exc_info:
         await user_service.update_user(user_update, user_token)
         assert exc_info.value.status_code == 404
