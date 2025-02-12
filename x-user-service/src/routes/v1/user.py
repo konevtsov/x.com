4ab97e6caa9424c6ee0a5fdd0f89a3fac8b721a8
Egor Konevtsov <konevtsov.code@gmail.com>
2025-02-12 23:50:17 +0700

    feat(user): Add views for avatar post, get, delete

--- x-user-service/src/routes/v1/user.py
+++ x-user-service/src/routes/v1/user.py
@@ -1,11 +1,18 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, UploadFile, HTTPException, status
+from fastapi.responses import StreamingResponse
 
 from schemas.user import (
+    FollowSchema,
+    UnfollowSchema,
+    AvatarUploadSchema,
+    UserUpdateSchema,
+    DeleteAvatarSchema,
+    GetAvatarSchema,
+)
+from .user_schemas import (
     UserUpdateRequestSchema,
     FollowRequestSchema,
-    FollowSchema,
     UnfollowRequestSchema,
-    UnfollowSchema,
 )
 from services.user_service import UserService
 from .token_introspection import get_token_info_from_current_user
@@ -75,6 +82,51 @@ async def unfollow(
         follower_id=user_token.user_id,
     )
     await user_service.unfollow(unfollow_schema=unfollow_schema)
+
+
+@router.post(
+    path="/avatar",
+)
+async def upload_avatar(
+    file: UploadFile,
+    user_token: TokenIntrospectSchema = Depends(get_token_info_from_current_user),
+    user_service: UserService = Depends(UserService),
+):
+    allowed_content_types = ["image/jpeg"]
+    if file.content_type not in allowed_content_types:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Unsupported Content-Type")
+
+    content = await file.read()
+    if not content:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="File is empty")
+
+    file_extension = str(file.content_type.split('/')[1])
+    upload_avatar_schema = AvatarUploadSchema(content=content, file_extension=file_extension)
+    await user_service.upload_avatar(upload_schema=upload_avatar_schema, user_token=user_token)
+
+
+@router.get(
+    path="/{username}/avatar",
+)
+async def get_avatar(
+    username: str,
+    user_token: TokenIntrospectSchema = Depends(get_token_info_from_current_user),
+    user_service: UserService = Depends(UserService),
+):
+    get_avatar_schema = GetAvatarSchema(username=username)
+    data = await user_service.get_avatar_by_username(get_avatar_schema)
+    return StreamingResponse(iter([data]), media_type="image/jpeg")
+
+
+@router.delete(
+    path="/avatar",
+)
+async def delete_avatar(
+    user_token: TokenIntrospectSchema = Depends(get_token_info_from_current_user),
+    user_service: UserService = Depends(UserService),
+):
+    delete_avatar_schema = DeleteAvatarSchema(user_id=user_token.user_id)
+    await user_service.delete_avatar(delete_avatar_schema)
 
 
 @router.put(
