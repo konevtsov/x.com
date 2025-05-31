aa7d6f162a7a52ed2b67951ed2694f060c7e4acf
Egor Konevtsov <konevtsov.connect@gmail.com>
2025-05-31 18:53:43 +0700

    feat: Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,19 @@
 #!/bin/sh
 
-set -e 
+set -e
 
 for dir in infrastructure x-auth-service x-user-service x-post-service
-do 
-  (cd "$dir" && docker compose up -d)
+do
+  (
+    cd "$dir"
+    if docker compose ps -q | grep -q .; then
+      docker compose restart
+    else
+      docker compose up -d
+    fi
+    sleep 2
+  )
 done
 
 echo "Done!"
+
